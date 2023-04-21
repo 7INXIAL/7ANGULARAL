16e19bd02fa7731ab8b07b74f8f22b51f20bc93c
7INXIAL <7INXIAL@gmail.com>
2023-04-21 16:47:31 +0800

    feat: 更改

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { GlobalComponent } from './community/global/global.component';
-
 @NgModule({
-  declarations: [
-    GlobalComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [GlobalComponent]
+  declarations: [GlobalComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  // providers: [{ provide: LOCALE_ID, useValue: 'zh' }],
+  bootstrap: [GlobalComponent],
 })
-export class AppModule { }
+export class AppModule {}
